64b4e0365d3ecf5335aea5ee4b2799bd031b52d9
deanbunn <deanbunn@gmail.com>
2025-03-14 14:02:37 -0700

    20250314_1402hrs base template

--- sintijazzapp/views.py
+++ sintijazzapp/views.py
@@ -1,10 +1,9 @@
-from django.http import HttpResponse
+#from django.http import HttpResponse
 from django.shortcuts import render
 
 
-# Create your views here.
 def index(request):
-    data = ["Oatmeal", "Grits", "Yogurt", "Cottage Cheese"]
+    data = ["Oatmeal", "Grits", "Yogurt", "Cottage Cheese", "Eggs"]
     context = {"foods": data}
     return render(request,"sintijazzapp/index.html",context)
 
