9dc53941d6ff55d60ab1794fffdd4339f998c50b
deanbunn <deanbunn@gmail.com>
2025-03-14 10:31:52 -0700

    20250314_1031hrs first template

--- sintijazzapp/views.py
+++ sintijazzapp/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello Coders!")
+
+    return render(request,"sintijazzapp/index.html")
