39c751a95a42ef07df14e2e9010c1cc28e3d3bd8
deanbunn <deanbunn@gmail.com>
2025-03-24 21:10:20 -0700

    20250324_2110hrs record size

--- sintijazzapp/views.py
+++ sintijazzapp/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 def index(request):
     data = ["Oatmeal", "Grits", "Yogurt", "Cottage Cheese", "Eggs"]
-    context = {"foods": data}
+    context = {"foods": data, "record_size": 45}
     return render(request,"sintijazzapp/index.html",context)
 
 
