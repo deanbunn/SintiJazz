b57b74aacf5fc549b31e89f7eac2e6e114ebbeb7
deanbunn <deanbunn@gmail.com>
2025-05-08 21:56:11 -0700

    20250508_2156hrs first model

--- sintijazzapp/models.py
+++ sintijazzapp/models.py
@@ -3,11 +3,13 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-class Musician(models.Model):
-    display_name = models.CharField(max_length=200)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+class Artist(models.Model):
+    name_first = models.CharField(max_length=100)
+    name_last = models.CharField(max_length=100)
+    name_display = models.CharField(max_length=200)
       
     def __str__(self):
-        return self.display_name
+        return self.name_display
+    
+    
     
